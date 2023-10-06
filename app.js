f040b814e5a1afa6eed43e70093e76d5d691abb0
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-06 10:50:45 +0900

    함수 안에 함수를 부르는 형식으로 시간이 지난 후 값 표시

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //1000마다 1초
+// setTimeout(function() {
+// console.log('첫번째 소사');
+// }, 3000)
 
 
 setTimeout(function() {
   console.log('첫번째 소사');
-}, 2000)
+  setTimeout(function() {
+    console.log('두번째 뭉기');
+    setTimeout(function() {
+      console.log('세번째 밍구');
+    }, 1000);
+  }, 2000);
 
-setTimeout(function() {
-  console.log('두번째 뭉기');
-}, 4000)
+}, 3000);
 
-setTimeout(function() {
-  console.log('세번째 밍구');
-}, 6000)
+
+
