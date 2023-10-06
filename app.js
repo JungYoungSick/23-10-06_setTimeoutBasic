7aebb5808f4a97ebe96d5ca7ccb9c9d2791c96a5
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-06 10:38:45 +0900

    setTimeout 시간차 콘솔 작성

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello")
+//1000마다 1초
+
+
+setTimeout(function() {
+  console.log('첫번째 소사');
+}, 2000)
+
+setTimeout(function() {
+  console.log('두번째 뭉기');
+}, 4000)
+
+setTimeout(function() {
+  console.log('세번째 밍구');
+}, 6000)
