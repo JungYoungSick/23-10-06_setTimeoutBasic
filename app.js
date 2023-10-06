7583d0913c268240b8ae75d450c0900d794d8563
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-06 11:32:27 +0900

    새 setInterval 사용 무한 반복 루프

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 
 
 
-function sosa() {
-  setTimeout(function() {
-    console.log('소사');
-    sosa();
-  }, 1000);
-};
+// function sosa() {
+//   setTimeout(function() {
+//     console.log('소사');
+//     sosa();
+//   }, 1000);
+// };
 
-sosa();
+// sosa();
+
+setInterval(function() {}, 1000);
+setInterval(function() {
+  console.log("소사 이따 향마루 ㄱ?")
+}, 1000);
